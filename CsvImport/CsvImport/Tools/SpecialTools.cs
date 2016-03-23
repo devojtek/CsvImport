6283b3d768ba15e26b892878b915042684e7c5f7
p.pobelle <p.pobelle@id2i.net>
2016-03-23 09:47:25 +0100

    forgotten file

---
+++ CsvImport/CsvImport/Tools/SpecialTools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pkshetlie.Csv.Import.Tools
+{
+    public static class SpecialTools
+    {
+        public static void Merge<TObject>(this TObject target, TObject source)
+        {
+            Type t = typeof(TObject);
+            var properties = t.GetProperties().Where(prop => prop.CanRead && prop.CanWrite);
+            foreach (var prop in properties)
+            {
+                var value = prop.GetValue(source, null);
+                if (value != null)
+                {
+                    prop.SetValue(target, value, null);
+                }
+            }
+        }
+
+    }
+}
